b329b93a80569cba59fbb8acb18ac0a9a6d343e8
Alexis Fox <alexis.fox@duke.edu>
2025-09-03 19:15:27 -0400

    fix to use gemini

--- model_adapters/__init__.py
+++ model_adapters/__init__.py
@@ -3,4 +3,4 @@ from .openai_adapter import OpenAIAdapter
 from .gemini_adapter import GeminiAdapter
 from .claude_adapter import ClaudeAdapter
 from .qwen_vl_adapter import QwenVLAdapter
-from .llava_adapter import LlavaAdapter
+# from .llava_adapter import LlavaAdapter
